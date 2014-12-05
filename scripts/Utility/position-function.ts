4a2aa99176e69aca3728d1e76dd1113c832a0eb6
4lk4tr43 <4lk4tr43@gmail.com>
2014-12-05 16:06:37 +0100

    PositionFunction wrapping

--- scripts/Utility/position-function.ts
+++ scripts/Utility/position-function.ts
@@ -25,9 +25,17 @@ module Utility {
          * @param positionFunction , the function to use for the y position
          * @param xOffset , offset for the input
          * @param yOffset , offset for the output
+         * @param xMin , min unwrapped value inclusive
+         * @param xMax , max unwrapped value inclusive
+         * @param yMin , min unwrapped value inclusive
+         * @param yMax , max unwrapped value inclusive
+         * @param wrappingEvent , fired in cas of wrapping e.x/e.y -> -1 lower, 0 not wrapped, 1 upper,
          */
-        constructor(public positionFunction:(x:number)=>number, public xOffset:number=0, public yOffset:number=0) {
-
+        constructor(public positionFunction:(x:number)=>number,
+                    public xOffset:number=0, public yOffset:number=0,
+                    public xMin:number=undefined, public xMax:number=undefined,
+                    public yMin:number=undefined, public yMax:number=undefined,
+                    public wrappingEvent:(e)=>void=undefined) {
         }
 
         /**
@@ -36,7 +44,47 @@ module Utility {
          * @returns {number} , y axis
          */
         getY(x:number):number {
-            return this.positionFunction(x + this.xOffset) + this.yOffset;
+            if (this.xMin !== undefined || this.xMax !== undefined ||
+                this.yMin !== undefined || this.yMax !== undefined) {
+
+                if (this.xMin === undefined) this.xMin = Number.MIN_VALUE;
+                if (this.xMax === undefined) this.xMax = Number.MAX_VALUE;
+                if (this.yMin === undefined) this.yMin = Number.MIN_VALUE;
+                if (this.yMax === undefined) this.yMax = Number.MAX_VALUE;
+
+                var eventArgs = {x:0, y:0};
+                var cappedX = x;
+
+                if (cappedX < this.xMin) {
+                    var distance = this.xMax - this.xMin;
+                    eventArgs.x = -1;
+                    cappedX = this.xMax + ((x + this.xMax) % distance);
+                }
+                else if (cappedX > this.xMax) {
+                    var distance = this.xMax - this.xMin;
+                    eventArgs.x = 1;
+                    cappedX = this.xMin + ((x - this.xMin) % distance);
+                }
+
+                var y = this.positionFunction(cappedX);
+
+                if (y < this.yMin) {
+                    var distance = this.yMax - this.yMin;
+                    eventArgs.y = -1;
+                    y = this.yMax - (y % distance);
+                }
+                else if (y > this.yMax) {
+                    var distance = this.yMax - this.yMin;
+                    eventArgs.y = 1;
+                    y = this.yMin + (y % distance);
+                }
+
+                if ((eventArgs.x !== 0 || eventArgs.y !== 0) && this.wrappingEvent !== undefined)
+                    this.wrappingEvent(eventArgs);
+
+                return y;
+            }
+            else return this.positionFunction(x + this.xOffset) + this.yOffset;
         }
     }
 }
--- tests/Utility/postition-function-test.js
+++ tests/Utility/postition-function-test.js
@@ -1,18 +1,47 @@
 TestCase('PositionFunction', {
-   testQuadratic: function () {
-       var quadraticFunction = Utility.PositionFunction.createQuadraticFunction(1,0,0);
-       var quadraticPosition = new Utility.PositionFunction(quadraticFunction);
+    testQuadratic: function () {
+        var quadraticFunction = Utility.PositionFunction.createQuadraticFunction(1,0,0);
+        var quadraticPosition = new Utility.PositionFunction(quadraticFunction);
 
-       assertEquals(0, quadraticPosition.getY(0));
-       assertEquals(1, quadraticPosition.getY(1));
-       assertEquals(4, quadraticPosition.getY(2));
-       assertEquals(4, quadraticPosition.getY(-2));
+        assertEquals(0, quadraticPosition.getY(0));
+        assertEquals(1, quadraticPosition.getY(1));
+        assertEquals(4, quadraticPosition.getY(2));
+        assertEquals(4, quadraticPosition.getY(-2));
 
-       quadraticPosition.xOffset = 1;
-       quadraticPosition.yOffset = -1;
-       assertEquals(0, quadraticPosition.getY(0));
-       assertEquals(3, quadraticPosition.getY(1));
-       assertEquals(8, quadraticPosition.getY(2));
-       assertEquals(0, quadraticPosition.getY(-2));
-   }
+        quadraticPosition.xOffset = 1;
+        quadraticPosition.yOffset = -1;
+        assertEquals(0, quadraticPosition.getY(0));
+        assertEquals(3, quadraticPosition.getY(1));
+        assertEquals(8, quadraticPosition.getY(2));
+        assertEquals(0, quadraticPosition.getY(-2));
+    },
+    testCapping: function () {
+        var quadraticFunction = Utility.PositionFunction.createQuadraticFunction(1,0,0);
+
+        var wrapped = false;
+        var wrapHandler = function (e) { wrapped = true; };
+
+        var quadraticPosition = new Utility.PositionFunction(quadraticFunction, 0, 0, -2, 2, undefined, undefined,
+            wrapHandler);
+
+        assertEqualsDelta(1, quadraticPosition.getY(1), 0.001);
+        assertFalse(wrapped);
+
+        assertEqualsDelta(1, quadraticPosition.getY(3), 0.001);
+        assertTrue(wrapped);
+        wrapped = false;
+        assertEqualsDelta(1, quadraticPosition.getY(-3), 0.001);
+        assertTrue(wrapped);
+        wrapped = false;
+
+        quadraticPosition.xMin = -4;
+        quadraticPosition.xMax = -2;
+        assertEqualsDelta(9, quadraticPosition.getY(-1), 0.001);
+        assertEqualsDelta(9, quadraticPosition.getY(-5), 0.001);
+
+        quadraticPosition.yMin = -4;
+        quadraticPosition.yMax = 4;
+        assertEquals(-3, quadraticPosition.getY(-1));
+        assertEquals(4, quadraticPosition.getY(-2));
+    }
 });
